3cb37fbb7df39c6f88cb56c4a7ff7889ed439370
tamara-taylor <ttaylor8522@gmail.com>
2021-07-23 19:09:33 -0400

    updated

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -6,3 +6,7 @@ const schema = yup.object().shape({
     username: yup.string().required('username is required').min('name must be at least 6 characters'),
     password: yup.string().required('password is required').min('name must be at least 6 characters'),
 })
+
+export default function Home (props) {
+    const { values,  }
+}
--- app/src/components/Register.js
+++ app/src/components/Register.js
@@ -5,6 +5,8 @@ import axios from 'axios'
 const schema = yup.object().shape({
     firstname: yup.string().required('first name is required').min('name must be at least 2 characters'),
     lastname: yup.string().required('last name is required').min('name must be at least 2 characters'),
+    male: yup.boolean().oneOf([true, false], ''),
+    female: yup.boolean().oneOf([true, false], ''),
     email: yup.string().required('email is required').min('email must be at least 6 characters'),
     phone: yup.string().required('phone is required'),
     address: yup.string().required('address is required'),
@@ -12,97 +14,126 @@ const schema = yup.object().shape({
     income: yup.string().required('income is required')
 })
 
-export default function Register (props){
-    const { values, submit, errors, disabled, form } = props
-    
-    const onChange = event => {
-        const { text, value, name, type } = event.target
-        const valueToUse = type === 'text' ? text : value
-        setFormErrors(name, valueToUse)
-        setForm({ ...Form, [name] : valueToUse })
-    }
-    
+export default function Register (){
+    const [form, setForm] = useState ({
+        firstname: '',
+        lastname: '',
+        male: false,
+        female: false,
+        email: '',
+        phone: '',
+        address: ''
+    })
+
+    const [error, setError] = useState({
+        firstname: '',
+        lastname: '',
+        male: '',
+        female: '',
+        email: '',
+        phone: '',
+        address: ''
+    })
+
+    const [user, setUser] = useState({
+        setForm
+    })
+
     const onSubmit = event => {
         event.preventDefault()
-        submit()
-    }
-    
-    const { form, setForm } = useState([])
-    const { formErrors, setFormErrors } = useState(FormErrors)
-    const { formValues, setFormValues } = useState(FormValues)
-    const { disabled, setDisabled } = useState(true)
+        const newUser = {
+            fistname: form.firstname,
+            lastname: form.lastname,
+            male: form.male,
+            female: form.female,
+            email: form.email,
+            phone: form.phone,
+            address: form.address
+        }
 
-    const newFormErrors = (name, value) => {
-        yup.reach(schema, name).validate(value)
-        .then(() => setFormErrors({ ...formErrors, [firstname]: '' }))
-        .catch(err => setFormErrors({ ...formErrors, [firstname]: errors.errors[0] }))
-        setFormValues({ ...formValues, [name]: value })
-    }
-  
-
-    const newForm = (newForm) => {
-        axios.post('https://reqres.in/api/orders', newForm)
+        axios.post('https://reqres.in/api/users', newUser)
         .then(res => {
-            setForm([ ...form, res.data ])
-            setFormValues(FormValues)
-        })
-      }
-    
-      const OnSubmit = event => {
-        const newUser = { 
-          firstname: formValues.firstname.trim(), 
-          lastname: formValues.lastname.trim(), 
-          email: formValues.email, 
-          phone: formValues.phone, 
-          address: formValues.address, 
-          ssn: formValues.ssn, 
-          income: formValues.income }
-    }
-    console.log('You are registered!', newUser)
-    chooseNewOrder(newUser)
-    setFormValues(FormValues)
+            setForm({
+                firstname: '',
+                lastname: '',
+                male: false,
+                female: false,
+                email: '',
+                phone: '',
+                address: ''
+            })
 
-    useEffect(() => {
-        schema.isValid(formValues).then(valid => setDisabled(!valid))
-    }, [formValues])
+            setUser({
+                firstname: form.firstname,
+                lastname: form.lastname,
+                male: form.male,
+                female: form.female,
+                email: form.email,
+                phone: form.phone,
+                address: form.address
+            })
+        })
+        .catch(error => {
+        })
+    }
+
+    const setFormErrors = (name,value) => {
+        yup.reach(schema, name).validate(value)
+        .then(() => setError({...error, [name]: ''}))
+        .catch(error => setError({...error, [name]: error.errors[0]}))
+    }
+
+    const onChange = evt => {
+        const { name, value, checked, type } = evt.target
+        const valueToUse = type === 'checkbox' ? checked : value
+        setFormErrors(name, valueToUse)
+        setForm({...form, [name]: valueToUse})
+    }
 
     return(
         <div className= 'container'>
-            <div className= 'head'></div>
+            <div className= 'head'>
+                <h1>Use My Tech Stuff</h1>
+                <div className='Navigation'>
+                <Link to='/'>Home</Link>
+                </div>
+            </div>
             <form id='register-form' onSubmit={onSubmit}>
                 <div>{errors.firstname}</div><div>{errors.lastname}</div><div>{errors.email}</div><div>{errors.phone}</div><div>{errors.address}</div><div>{errors.ssn}</div><div>{errors.income}</div>
-            </form>
             <div>
             <label>First Name
-                <input onChange={onChange} value={values.firstname} name='firstname' type='text' />
+                <input onChange={onChange} value={form.firstname} name='firstname' type='text' />
             </label>
             <label>Last Name
-                <input onChange={onChange} value={values.lastname} name='lastname' type='text' />
+                <input onChange={onChange} value={form.lastname} name='lastname' type='text' />
+            </label>
+            <label>Male
+                <input onChange={onChange}  name='male' type='checkbox' checked={form.male} />
+            </label>
+            <label>Female
+                <input onChange={onChange} name='male' type='checkbox' checked={form.female} />
             </label>
             <label>Email
-                <input onChange={onChange} value={values.email} name='email' type='text' />
+                <input onChange={onChange} value={form.email} name='email' type='text' />
             </label>
             <label>Phone
-                <input onChange={onChange} value={values.phone} name='phone' type='text' />
+                <input onChange={onChange} value={form.phone} name='phone' type='text' />
             </label>
             <label>Address
-                <input onChange={onChange} value={values.address} name='address' type='text' />
+                <input onChange={onChange} value={form.address} name='address' type='text' />
             </label>
             <label>SSN
-                <input onChange={onChange} value={values.ssn} name='ssn' type='text' />
+                <input onChange={onChange} value={form.ssn} name='ssn' type='text' />
             </label>
             <label>Income
-                <input onChange={onChange} value={values.income} name='income' type='text' /> 
+                <input onChange={onChange} value={form.income} name='income' type='text' /> 
             </label>
             <div className='form-style-button'>
                 <button disabled={disabled} id='register-button'>Submit</button>
             </div>
         </div>
-        </div>
+     </form>
+    </div>
     )
   
 }
-
-
-
-
